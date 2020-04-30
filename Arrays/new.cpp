cf389fbffc07d2f38744c36fad68752d2f06edee
curiex <vishnupillai1999@gmail.com>
2020-04-30 13:57:48 +0530

    new file

--- Arrays/new.cpp
+++ Arrays/new.cpp
@@ -1,7 +1,15 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
 
-int main() {
-	int n; std::cin >> n;
-	std::cout << n;
+int32_t main() {
+#ifndef ONLINE_JUDGE
+	freopen( "C:/Users/Home/Desktop/c++/input.txt", "r" , stdin);
+	freopen( "C:/Users/Home/Desktop/c++/output.txt", "w" , stdout);
+#endif
+	int P[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+	for (int i = 0; i < 10; i++) {
+		cout << *(P + i) << " ";
+	}
 	return 0;
 }
